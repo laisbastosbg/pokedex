22f4f311f34a824bbecef9b3927cd0d05700b0bc
Lais Godinho <lais2bg@gmail.com>
2022-05-02 09:01:11 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Lais Godinho on 02/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
